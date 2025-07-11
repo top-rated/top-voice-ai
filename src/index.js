499be03d9c5f0f9866e650f33da89a5fbeb9ecc6
Rahees Ahmed <raheesahmed256@gmail.com>
2025-07-12 02:33:22 +0500

    fix: body-parser middleware dependency

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
 const path = require("path");
+const bodyParser = require("body-parser");
 
 // Import routes
 const authRoutes = require("./routes/auth.routes");
